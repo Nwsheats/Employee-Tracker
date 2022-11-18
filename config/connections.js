60f8545e24c29658701a25eb25d6970c7700db82
Nwsheats <nwsheats@gmail.com>
2022-11-17 22:04:05 -0600

    removed sequelize and express, changed mysql2 info and env file, renamed server to index

--- config/connections.js
+++ config/connections.js
@@ -1,15 +1,21 @@
-const Sequelize = require('sequelize');
+const mysql = require('mysql2');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
-  }
-);
 
-module.exports = sequelize;
+const connection = mysql.createConnection(
+    {
+      host: 'localhost',
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME
+    },
+    console.log(`Connected to the database.`)
+  );
+
+connection.connect((err) => {
+    if (err) {
+        throw err;
+    }
+}) 
+
+module.exports = connection;
