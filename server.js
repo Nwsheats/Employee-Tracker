e377b237dcd51b47946478ee106cbcb556f2e95b
Nwsheats <nwsheats@gmail.com>
2022-11-17 21:40:37 -0600

    added sequelize and dotenv, added .env and .env.example

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const mysql = require('mysql2');
+const sequelize = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,15 +7,6 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-const db = mysql.createConnection(
-  {
-    host: 'localhost',
-    user: 'root',
-    password: 'Gr0mh3llscream',
-    database: 'employee_db'
-  },
-  console.log(`Connected to the database.`)
-);
 
 // add apiroutes
 
