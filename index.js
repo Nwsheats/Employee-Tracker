8dbaeb05dd5ea39862d827b64d935915b8214c17
Nwsheats <nwsheats@gmail.com>
2022-11-21 19:45:58 -0600

    added switch statement

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
-const cTable = require('console.table')
+const cTable = require('console.table');
+const { allowedNodeEnvironmentFlags } = require('process');
 
 
 const getEmployees = () => {
@@ -15,5 +16,36 @@ const inquiry = () => {
             name: 'initial',
             choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department', 'Quit']
         }
-    ])
+    ]) .then((data) => {
+        switch (data) {
+            case data.choices === 'View All Employees':
+                db.query('SELECT * FROM employee', function (err, results) {
+                    console.table(results);
+                })
+                return inquiry();
+            case data.choices === 'Add Employee':
+                // code
+                return addEmployee();
+            case data.choices === 'Update Employee Role':
+                //code
+                return updateRole();
+            case data.choices === 'View All Roles':
+                db.query('SELECT * FROM roles', function (err, results) {
+                    console.table(results);
+                })
+                return inquiry();
+            case data.choices === 'Add Role':
+                // code
+                return addRole();
+            case data.choices === 'View All Departments':
+                db.query('SELECT * FROM department', function (err, results) {
+                    console.table(results);
+                })
+            case data.choices === 'Add Department':
+                // code
+                return addDept();
+            case data.choices === 'Quit':
+                break;
+        }
+    })
 }
