67492abc35df673d217de7cda92f8d39c3905485
Nwsheats <nwsheats@gmail.com>
2022-11-21 18:34:25 -0600

    added options to inquirer list

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const inquiry = () => {
             type: 'list',
             message: 'What would you like to do?',
             name: 'initial',
-            choices: ''
+            choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department', 'Quit']
         }
     ])
 }
